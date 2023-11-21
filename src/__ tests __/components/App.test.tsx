eaaf70a7107f4172d2dbff7b6998818a3083d0fa
Oliver Harris <revilosirrah@gmail.com>
2023-11-21 17:25:06 +0000

    Minor updates

--- src/__ tests __/components/App.test.tsx
+++ src/__ tests __/components/App.test.tsx
@@ -1,9 +1,14 @@
-import { render, screen } from "@testing-library/react";
+import { cleanup, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import App from "../../components/App";
 
-test("Renders the main page", () => {
-  render(<App />);
-  const buttonElement = screen.getByText(/Enter Site/i);
-  expect(buttonElement).toBeInTheDocument();
+// describe block to organise test
+describe("initial default tests", () => {
+  afterEach(cleanup);
+
+  test("Renders the main page", () => {
+    render(<App />);
+    const buttonElement = screen.getByText(/Enter Site/i);
+    expect(buttonElement).toBeInTheDocument();
+  });
 });
